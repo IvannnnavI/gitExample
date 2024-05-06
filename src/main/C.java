37bc9c8bb60e44ad2f6e8e27d40f06ab9e45fe28
Ivan Mishchenko <157916203+IvannnnavI@users.noreply.github.com>
2024-05-06 21:29:54 +0200

    Update C.java

--- src/main/C.java
+++ src/main/C.java
@@ -2,6 +2,8 @@ package my_package;
 
 public class C {
     public void printHello() {
-        System.out.println("Hello");
+        System.out.println("Hello!");
+        System.out.println("it's me");
+        System.out.println ("I was wondering if after all these years you'd like to meet ...")
     }
 }
